2633fc32f213ae24a1111f38562e9a2664fcf92c
Blitzkriegtech <aljunrendon@yahoo.com>
2025-07-08 10:23:34 -0400

    add styles using partials

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+import "@hotwired/turbo-rails"
+import "bootstrap"
+
+
+// Test Bootstrap availability
+// document.addEventListener("turbo:load", () => {
+//   console.log("Turbo loaded, Bootstrap available:", typeof bootstrap !== 'undefined')
+// })
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("DOM loaded, Bootstrap available:", typeof bootstrap !== 'undefined')
+// })
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    # config.sass.preferred_syntax = :scss  # If you use .scss files
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
