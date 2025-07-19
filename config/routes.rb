751139be5b31a4dd44951d62aa761e4ce77f994e
Blitzkriegtech <aljunrendon@yahoo.com>
2025-07-19 05:37:56 -0400

    Add password reset feature

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [ :edit ]
+  resources :password_resets, only: [ :new, :create, :update, :edit ]
 
   get "up" => "rails/health#show", as: :rails_health_check
 
